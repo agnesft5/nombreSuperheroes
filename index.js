1561aa6680f02749b797e14b01c527913c811359
Agnes Font <agnesft5@gmail.com>
2019-10-16 16:29:55 +0200

    Retoques finales

--- index.js
+++ index.js
@@ -9,15 +9,18 @@ function manNameGenerator(event){
     let adj = ["Imperial", "Amazing", "Spectular", "Dino", "Massive", "Fantastic", "Wonder", "Techno", "Electro", "Hydro", "Giant", "Super", "Incredi", "Daring", "Mega", "Tiny", "Red", "Dark", "Orange", "Screaming", "Green", "Teal", "Blue", "Golden", "Fearless", "Great", "Ameri", "Pyro", "Robo", "American", "Cyber", "Frozen"];
     let sus = ["Spider", "Laser", "Microbe", "Spectre", "Scan", "Badger", "Lighting", "Thunder", "Eagle", "Hurricane", "Storm", "Typhoon", "Fire", "Flame", "Flash", "Night", "Whirlwind", "Wind", "Dawn", "Light", "Dragon", "Wolf", "Vemon", "Cobra", "Viper", "Condor", "Stalker", "Panther", "Puma", "Shadow", "Freeze", "Night", "Hammer", "Mist", "Tulip", "Octopus", "Inferno", "Magma", "Patriot", "Stag", "Rhino", "Mole", "Sloth"];
     resultado = (`${titleMas[numberGenerator(titleMas.length)]} ${adj[numberGenerator(adj.length)]} ${sus[numberGenerator(sus.length)]}`);
-    return resultado;
+    return resultado; //el numero aleatori generat va dins de cada array com a i.
 }
 
-let ManButton = document.querySelectorAll("body > button")[0];
-nameGeneratorButton.addEventListener("click", ()=>{
-    console.log(nameGenerator(event))});
+let manButton = document.querySelectorAll(".man__button")[0];
+
+manButton.addEventListener("click", ()=>{
+    let manName = manNameGenerator(event);
+    outputContainer.value = `${manName}`
+});
 
 //nom dona
-function WomanNameGenerator(event){
+function womanNameGenerator(event){
     let titleFem = ["Girl", "Woman", "Woman", "Woman", "Woman", "Gal", "Miss", "Doctor", "Professor", "Captain", "Agent", "The", "The", "The", "Sgt.", "General"];
     let adj = ["Imperial", "Amazing", "Spectular", "Dino", "Massive", "Fantastic", "Wonder", "Techno", "Electro", "Hydro", "Giant", "Super", "Incredi", "Daring", "Mega", "Tiny", "Red", "Dark", "Orange", "Screaming", "Green", "Teal", "Blue", "Golden", "Fearless", "Great", "Ameri", "Pyro", "Robo", "American", "Cyber", "Frozen"];
     let sus = ["Spider", "Laser", "Microbe", "Spectre", "Scan", "Badger", "Lighting", "Thunder", "Eagle", "Hurricane", "Storm", "Typhoon", "Fire", "Flame", "Flash", "Night", "Whirlwind", "Wind", "Dawn", "Light", "Dragon", "Wolf", "Vemon", "Cobra", "Viper", "Condor", "Stalker", "Panther", "Puma", "Shadow", "Freeze", "Night", "Hammer", "Mist", "Tulip", "Octopus", "Inferno", "Magma", "Patriot", "Stag", "Rhino", "Mole", "Sloth"];
@@ -25,6 +28,11 @@ function WomanNameGenerator(event){
     return resultado;
 }
 
-let nameGeneratorButton = document.querySelectorAll("body > button")[0];
-WomanButton.addEventListener("click", ()=>{
-    console.log(nameGenerator(event))});
+let womanButton = document.querySelectorAll(".woman__button")[0];
+womanButton.addEventListener("click", ()=>{
+    let womanName = womanNameGenerator(event);
+    outputContainer.value = `${womanName}`
+});
+
+//nom input
+let outputContainer = document.querySelectorAll(".body__input")[0];
